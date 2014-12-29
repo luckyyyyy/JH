63169e31d3a53fda6f884dce86c9b7405a16e856
Webster-jx3 <chenwei.web@hotmail.com>
2014-12-29 16:33:49 +0800

    修正XML错误

--- 0Base/Base.lua
+++ 0Base/Base.lua
@@ -2187,7 +2187,7 @@ _GUI.tItemXML = {
 	["Text"] = "<text>w=150 h=30 valign=1 font=162 eventid=257 </text>",
 	["Image"] = "<image>w=100 h=100 </image>",
 	["Animate"] = "<Animate>w=100 h=100 </Animate>",
-	["Box"] = "<box>w=48 h=48 eventid=525311 </text>",
+	["Box"] = "<box>w=48 h=48 eventid=525311 </box>",
 	["Shadow"] = "<shadow>w=15 h=15 eventid=277 </shadow>",
 	["Handle"] = "<handle>w=10 h=10</handle>",
 	["Label"] = "<handle>w=150 h=30 eventid=257 <text>name=\"Text_Label\" w=150 h=30 font=162 valign=1 </text></handle>",
