6fac8925ec87e5e0c7e2f514db27d878ff99a43a
ÇàÑô»ê <zjmail.chen@gmail.com>
2015-01-02 16:24:57 +0800

    新增系统字符串给予参考，修正部分内容。

--- 7zip.bat
+++ 7zip.bat
@@ -9,7 +9,7 @@ set szTime=%szTime: =%
 :: 拼接字符串开始压缩文件
 set szFile=releases\JH_%szTime%.7z
 echo zippping...
-7z a -t7z %szFile% -xr!manifest.dat -xr!manifest.key -xr!publisher.key -x@7zipignore.txt -xr!About.ini -xr!RGES-* -xr!sync_*
+7z a -t7z %szFile% -xr!manifest.dat -xr!string.txt -xr!string.lua -xr!manifest.key -xr!publisher.key -x@7zipignore.txt -xr!About.ini -xr!RGES-* -xr!sync_*
 echo File(s) compressing acomplete!
 echo Url: %szFile%
 set /p _=press enter to exit...
