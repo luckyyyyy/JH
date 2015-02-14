1ff8b01318bf667e1dd90bad42b1a624bd8db1ef
ÇàÑô»ê <zjmail.chen@gmail.com>
2015-02-14 19:53:55 +0800

    错误修正

--- RaidGrid_CTM_Edition/RaidGrid_Party.lua
+++ RaidGrid_CTM_Edition/RaidGrid_Party.lua
@@ -679,7 +679,8 @@ function CTM:RefresBuff()
 				for i = 0, handle:GetItemCount() - 1 do
 					if p then
 						local h = handle:Lookup(i)
-						local _, dwID, nLevel = h:Lookup("Box"):GetObject()
+						local hBox = hBuff:Lookup("Box")
+						local _, dwID, nLevel = hBox:GetObject()
 						local bExist, tBuff = JH.HasBuff(dwID)
 						if bExist then
 							local nTime = JH.GetEndTime(tBuff.nEndFrame)
