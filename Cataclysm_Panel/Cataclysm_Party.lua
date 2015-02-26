3fd8f4640eee81a9628989e9922cf97568fbd79e
Webster-jx3 <chenwei.web@hotmail.com>
2015-02-26 15:19:14 +0800

    字体设置确实麻烦点

--- Cataclysm_Panel/Cataclysm_Party.lua
+++ Cataclysm_Panel/Cataclysm_Party.lua
@@ -1,7 +1,7 @@
 -- @Author: Webster
 -- @Date:   2015-01-21 15:21:19
 -- @Last Modified by:   Webster
--- @Last Modified time: 2015-02-26 13:06:47
+-- @Last Modified time: 2015-02-26 15:15:53
 local _L = JH.LoadLangPack
 -----------------------------------------------
 -- 重构 @ 2015 赶时间 很多东西写的很粗略
