2b0a442945be747073ee3da352cfbe7e9314b3fa
青阳魂 <zjmail.chen@gmail.com>
2015-03-02 18:13:06 +0800

    修改shadow的前后顺序

--- Cataclysm_Panel/Cataclysm_Main.lua
+++ Cataclysm_Panel/Cataclysm_Main.lua
@@ -1,7 +1,7 @@
 -- @Author: Webster
 -- @Date:   2015-01-21 15:21:19
 -- @Last Modified by:   Webster
--- @Last Modified time: 2015-03-02 01:54:12
+-- @Last Modified time: 2015-03-02 03:24:34
 local _L = JH.LoadLangPack
 local Station = Station
 local CTM_CONFIG = {
