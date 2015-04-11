5175b644bcec290c4191b41964ac0c5349190c92
Webster-jx3 <chenwei.web@hotmail.com>
2015-04-12 01:00:02 +0800

    Cataclysm团队面板补上工资结算的确认提示

--- AutoSetTeam/AutoSetTeam.lua
+++ AutoSetTeam/AutoSetTeam.lua
@@ -1,7 +1,7 @@
 -- @Author: Webster
 -- @Date:   2015-01-21 15:21:19
 -- @Last Modified by:   Webster
--- @Last Modified time: 2015-04-04 14:43:54
+-- @Last Modified time: 2015-04-12 00:23:53
 local _L = JH.LoadLangPack
 JH_AutoSetTeam = {
 	bAppendMark = true,
