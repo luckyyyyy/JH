ecf860b3856efb3c7c66efd1452a24b7566322ec
青阳魂 <zjmail.chen@gmail.com>
2015-04-27 15:46:12 +0800

    减少lookup次数

--- 0Base/Base.lua
+++ 0Base/Base.lua
@@ -1,7 +1,7 @@
 -- @Author: Webster
 -- @Date:   2015-01-21 15:21:19
 -- @Last Modified by:   Webster
--- @Last Modified time: 2015-04-24 23:43:23
+-- @Last Modified time: 2015-04-27 15:41:18
 ---------------------------------------------------------------------
 -- ∂‡”Ô—‘¥¶¿Ì
 ---------------------------------------------------------------------
@@ -9,7 +9,7 @@ local ROOT_PATH   = "interface/JH/0Base/"
 local DATA_PATH   = "interface/JH/@DATA/"
 local SHADOW_PATH = "interface/JH/0Base/item/shadow.ini"
 local ADDON_PATH  = "interface/JH/"
-local _VERSION_   = 0x0090502
+local _VERSION_   = 0x00a0000
 local function GetLang()
 	local _, _, szLang = GetVersion()
 	local t0 = LoadLUAData(ROOT_PATH .. "lang/default.jx3dat") or {}
