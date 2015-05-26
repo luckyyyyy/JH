234e6fb63e7f0b04a66e0819c579d49eb8203ed9
青阳魂 <zjmail.chen@gmail.com>
2015-05-27 01:30:22 +0800

    Release 1.0.1b2

--- 0Base/Base.lua
+++ 0Base/Base.lua
@@ -1,7 +1,7 @@
 -- @Author: Webster
 -- @Date:   2015-01-21 15:21:19
 -- @Last Modified by:   Webster
--- @Last Modified time: 2015-05-26 19:50:27
+-- @Last Modified time: 2015-05-27 01:30:03
 ---------------------------------------------------------------------
 -- ∂‡”Ô—‘¥¶¿Ì
 ---------------------------------------------------------------------
@@ -22,7 +22,7 @@ local ROOT_PATH   = "interface/JH/0Base/"
 local DATA_PATH   = "interface/JH/@DATA/"
 local SHADOW_PATH = "interface/JH/0Base/item/shadow.ini"
 local ADDON_PATH  = "interface/JH/"
-local _VERSION_   = 0x1000101
+local _VERSION_   = 0x1000102
 local function GetLang()
 	local _, _, szLang = GetVersion()
 	local t0 = LoadLUAData(ROOT_PATH .. "lang/default.jx3dat") or {}
@@ -86,7 +86,7 @@ do
 end
 
 local _JH = {
-	szBuildDate    = "20150525",
+	szBuildDate    = "20150527",
 	szTitle        = _L["JH, JX3 Plug-in Collection"],
 	tHotkey        = {},
 	tAnchor        = {},
