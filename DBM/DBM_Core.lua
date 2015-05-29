e581df9e8a7099016f759bb26d723f7f173fda9f
ÇàÑô»ê <zjmail.chen@gmail.com>
2015-05-29 15:55:21 +0800

    修复一个倒计时BUG

--- DBM/DBM_Core.lua
+++ DBM/DBM_Core.lua
@@ -1,7 +1,7 @@
 -- @Author: Webster
 -- @Date:   2015-05-13 16:06:53
 -- @Last Modified by:   Webster
--- @Last Modified time: 2015-05-29 15:01:49
+-- @Last Modified time: 2015-05-29 15:46:47
 
 local _L = JH.LoadLangPack
 local ipairs, pairs = ipairs, pairs
