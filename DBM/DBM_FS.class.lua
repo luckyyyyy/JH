2f6331e3116acee4ae098d24583c7416b5271eed
Webster <chenwei.web@hotmail.com>
2015-09-02 16:08:08 +0800

    修复可能导致刷LOG的问题

--- DBM/DBM_FS.class.lua
+++ DBM/DBM_FS.class.lua
@@ -1,7 +1,7 @@
 -- @Author: Webster
 -- @Date:   2015-05-02 06:59:32
 -- @Last Modified by:   Webster
--- @Last Modified time: 2015-08-22 19:19:01
+-- @Last Modified time: 2015-09-02 16:06:01
 -- JX3_Client 全屏泛光类
 local FS = class()
 
@@ -102,17 +102,20 @@ function FS:ctor(szKey, tArgs)
 			ui = FS_HANDLE:AppendItemFromIni(FS_INIFILE, "Handle_Item")
 		end
 		ui.sha1 = ui.sha1 or ui:AppendItemFromIni(SHADOW, "shadow")
-		ui.sha1:SetTriangleFan(GEOMETRY_TYPE.TRIANGLE)
-		ui.sha1:SetD3DPT(D3DPT.TRIANGLESTRIP)
-
+		if ui.sha1:IsValid() then
+			ui.sha1:SetTriangleFan(GEOMETRY_TYPE.TRIANGLE)
+			ui.sha1:SetD3DPT(D3DPT.TRIANGLESTRIP)
+		end
 		ui.bFlash  = tArgs.bFlash
 		ui.nTime   = tArgs.nTime
 		ui.nCreate = nTime
 		ui.col     = tArgs.col or { 255, 128, 0 }
 		if tArgs.tBindBuff then
 			ui.sha2 = ui.sha2 or ui:AppendItemFromIni(SHADOW, "shadow")
-			ui.sha2:SetTriangleFan(GEOMETRY_TYPE.TRIANGLE)
-			ui.sha2:SetD3DPT(D3DPT.TRIANGLESTRIP)
+			if ui.sha2:IsValid() then
+				ui.sha2:SetTriangleFan(GEOMETRY_TYPE.TRIANGLE)
+				ui.sha2:SetD3DPT(D3DPT.TRIANGLESTRIP)
+			end
 			ui.tBindBuff = tArgs.tBindBuff
 		end
 		self.ui = ui
