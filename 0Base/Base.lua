a257fb7419382b5788b1aa13f75dcde976176af3
ÇàÑô»ê <zjmail.chen@gmail.com>
2015-10-08 12:37:19 +0800

    Release 1.0.8

--- 0Base/Base.lua
+++ 0Base/Base.lua
@@ -1,7 +1,7 @@
 -- @Author: Webster
 -- @Date:   2015-01-21 15:21:19
 -- @Last Modified by:   Webster
--- @Last Modified time: 2015-09-22 16:50:57
+-- @Last Modified time: 2015-10-08 12:01:21
 
 -- these global functions are accessed all the time by the event handler
 -- so caching them is worth the effort
@@ -19,7 +19,7 @@ local ROOT_PATH   = "interface/JH/0Base/"
 local DATA_PATH   = "interface/JH/@DATA/"
 local SHADOW_PATH = "interface/JH/0Base/item/shadow.ini"
 local ADDON_PATH  = "interface/JH/"
-local _VERSION_   = 0x1000700
+local _VERSION_   = 0x1000800
 
 ---------------------------------------------------------------------
 -- ∂‡”Ô—‘¥¶¿Ì
@@ -72,7 +72,7 @@ do
 end
 
 local _JH = {
-	szBuildDate  = "20150831",
+	szBuildDate  = "20151008",
 	szTitle      = _L["JH, JX3 Plug-in Collection"],
 	tHotkey      = {},
 	tAnchor      = {},
