8f55e9d101536c9f8874e3165f276e6105fbafe0
ÇàÑô»ê <zjmail.chen@gmail.com>
2015-10-29 19:37:48 +0800

    优化组队列表

--- RaidTools/RaidTools.lua
+++ RaidTools/RaidTools.lua
@@ -1,7 +1,7 @@
 -- @Author: ChenWei-31027
 -- @Date:   2015-06-19 16:31:21
 -- @Last Modified by:   Webster
--- @Last Modified time: 2015-10-21 14:30:33
+-- @Last Modified time: 2015-10-29 18:55:07
 
 local _L = JH.LoadLangPack
 
