4c080a6343936e4b6bddaf9914fc4bdb03334c59
青阳魂 <zjmail.chen@gmail.com>
2017-05-27 16:26:11 +0800

    disabled crazy module

--- JH_DBM/DBM_LT.class.lua
+++ JH_DBM/DBM_LT.class.lua
@@ -1,7 +1,7 @@
 -- @Author: Webster
 -- @Date:   2015-01-21 15:21:19
 -- @Last Modified by:   Administrator
--- @Last Modified time: 2017-05-27 16:13:04
+-- @Last Modified time: 2017-05-27 16:24:28
 local _L = JH.LoadLangPack
 
 DBM_LT = {
@@ -67,7 +67,7 @@ function LT.UpdateText(txt, col)
 	end
 	LT.txt:SetText(txt)
 	LT.txt:SetFontScheme(DBM_LT.dwFontScheme)
-	LT.txt:SetFontScale(DBM_LT.fScale)
+	LT.txt:SetFontScale(1.5)
 	LT.txt:SetFontColor(unpack(col))
 	LT.frame:FadeIn(0)
 	LT.frame:SetAlpha(255)
@@ -89,11 +89,11 @@ local PS = {}
 function PS.OnPanelActive(frame)
 	local ui, nX, nY = GUI(frame), 10, 0
 	nX, nY = ui:Append("Text", { x = 0, y = 0, txt = _L["LargeText"], font = 27 }):Pos_()
-	nX = ui:Append("Text", { txt = _L["Font Scale"], x = 10, y = nY + 10 }):Pos_()
-	nX, nY = ui:Append("WndTrackBar", { x = nX + 10, y = nY + 13, txt = "" }):Range(1, 1.5, 5):Value(DBM_LT.fScale):Change(function(nVal)
-		DBM_LT.fScale = nVal
-		ui:Fetch("preview"):Font(DBM_LT.dwFontScheme):Scale(DBM_LT.fScale)
-	end):Pos_()
+	-- nX = ui:Append("Text", { txt = _L["Font Scale"], x = 10, y = nY + 10 }):Pos_()
+	-- nX, nY = ui:Append("WndTrackBar", { x = nX + 10, y = nY + 13, txt = "" }):Range(1, 1.5, 5):Value(DBM_LT.fScale):Change(function(nVal)
+	-- 	DBM_LT.fScale = nVal
+	-- 	ui:Fetch("preview"):Font(DBM_LT.dwFontScheme):Scale(DBM_LT.fScale)
+	-- end):Pos_()
 
 	nX = ui:Append("Text", { txt = _L["Pause time"], x = 10, y = nY }):Pos_()
 	nX, nY = ui:Append("WndTrackBar", { x = nX + 10, y = nY + 3, txt = _L["s"] }):Range(0.5, 3, 25):Value(DBM_LT.fPause):Change(function(nVal)
@@ -108,13 +108,13 @@ function PS.OnPanelActive(frame)
 	nX = ui:Append("WndButton2", { x = 10, y = nY + 5, txt = g_tStrings.FONT }):Click(function()
 		GUI.OpenFontTablePanel(function(nFont)
 			DBM_LT.dwFontScheme = nFont
-			ui:Fetch("preview"):Font(DBM_LT.dwFontScheme):Scale(DBM_LT.fScale)
+			ui:Fetch("preview"):Font(DBM_LT.dwFontScheme):Scale(1.5)
 		end)
 	end):Pos_()
 	ui:Append("WndButton2", { txt = _L["preview"], x = nX + 10, y = nY + 5 }):Click(function()
 		LT.UpdateText(_L("%s are welcome to use JH plug-in", GetUserRoleName()))
 	end)
-	ui:Append("Text", "preview", { x = 20, y = nY + 50, txt = _L["JX3"], font = DBM_LT.dwFontScheme}):Scale(DBM_LT.fScale)
+	ui:Append("Text", "preview", { x = 20, y = nY + 50, txt = _L["JX3"], font = DBM_LT.dwFontScheme}):Scale(1.5)
 	nX, nY = ui:Append("Text", { txt = _L["Tips"], x = 0, y = 230, font = 27 }):Pos_()
 	nX, nY = ui:Append("Text", { x = 10, y = nY + 10, w = 500 , h = 20, multi = true, txt = _L["Enable KG3DEngineDX11 better effect"] }):Pos_()
 end
