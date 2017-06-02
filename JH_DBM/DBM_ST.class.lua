4ce1c16f2e600ccdc329b0c77992ca00a17460e3
青阳魂 <zjmail.chen@gmail.com>
2017-06-02 23:50:23 +0800

    Fixed Bugs

--- JH_DBM/DBM_ST.class.lua
+++ JH_DBM/DBM_ST.class.lua
@@ -1,7 +1,7 @@
 -- @Author: Webster
 -- @Date:   2015-04-28 16:41:08
 -- @Last Modified by:   Administrator
--- @Last Modified time: 2017-05-27 15:59:12
+-- @Last Modified time: 2017-06-02 23:48:31
 -- JX3_Client 倒计时类
 local _L = JH.LoadLangPack
 -- ST class
@@ -272,11 +272,7 @@ end
 -- 设置倒计时的名称和时间 用于动态改变分段倒计时
 function ST:SetInfo(tTime, nIcon)
 	if tTime.szName then
-		if wstring.len(tTime.szName) > 6 then
-			self.ui.txt:SetText("disabled")
-		else
-			self.ui.txt:SetText(tTime.szName)
-		end
+		self.ui.txt:SetText(wstring.sub(tTime.szName, 1, 7))
 	end
 	if tTime.nTime then
 		self.ui:SetUserData(math.floor(tTime.nTime))
